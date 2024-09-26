aa7866e3155a75e6b6db60ddce1cbb42f88f48f9
Jinsoo <jinchuu1391@gmail.com>
2024-09-26 11:38:41 +0900

    feat: 시작, 종료 상태 추가

--- src/app/api/actions.ts
+++ src/app/api/actions.ts
@@ -8,7 +8,7 @@ export type Params = {role: 'assistant' | 'user', content: string}
 export async function sendAnswer(messages: Params[]) {
   // TODO: system message 보강하기
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "너는 상담사야 꼬리질문을 통해 사용자가 지금 집중해야 할 중요한 한 가지가 뭔지 찾을 수 있도록 도와줘야해. 목표가 있다면 목표를 달성할 수 있도록, 목표가 없다면 현재 할 수 있는일에 최선을 다하도록 이끌어줘야해. 대화를 통해 지금 집중해야 할 단 한 가지 일을 찾아낸다면 격려의 말과 함께 대화를 끝내도록해" },
+    messages: [{ role: "system", content: "너는 상담사야 꼬리질문을 통해 사용자가 오늘 집중해야 할 중요한 한 가지가 뭔지 찾을 수 있도록 도와줘야해. 목표가 있다면 목표를 달성할 수 있도록, 목표가 없다면 현재 할 수 있는일에 최선을 다하도록 이끌어줘야해. 대화를 통해 지금 집중해야 할 단 한 가지 일을 찾아낸다면 격려의 말과 함께 대화를 끝내도록해. 대화가 끝났다고 판단되면 답변 앞에 {{END}} 를 붙여줘" },
       {role: 'assistant', content: '당신의 목표는 무엇인가요?'},
       ...messages],
     model: "gpt-4o-mini",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,99 @@
 "use client";
 
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, Fragment, KeyboardEvent, useMemo, useState } from "react";
 import { Params, sendAnswer } from "./api/actions";
 
 export default function Home() {
-  const [question, setQuestion] = useState("1. 당신의 목표는 무엇인가요?");
+  const [status, setStatus] = useState<'ready' | 'inProgress' | 'end'>('ready')
   const [answer, setAnswer] = useState('');
   const [histories, setHistories] = useState<Params[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const question = useMemo(() => {
+    if(histories.length === 0){
+      return '당신의 목표는 무엇인가요?'
+    }
+    const result = histories[histories.length - 1].content
+    return result.replace('{{END}}', '');
+  },[histories])
 
   const sendAnswerHandler = async () => {
     const res = await sendAnswer([...histories, {role: 'user', content: answer}])
     return res
   }
 
-  const onNextClick = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const onNextClick = async (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault()
     try {
       setLoading(true)
       const res = await sendAnswerHandler()
-      setQuestion(res.messages[res.messages.length-1].content ?? '')
+      const message = res.messages[res.messages.length-1].content
       setHistories(res.messages)
-      setAnswer('')
+      if(message.includes('{{END}}')){
+        setStatus('end')
+      }
     } catch (e) {
       console.error(e)
     } finally {
       setLoading(false)
+      setAnswer('')
     }
   };
 
   const answerChangeHandler = (e:ChangeEvent<HTMLTextAreaElement>) => {
     setAnswer(e.target.value)
   }
+
+  const onStartClick = () => {
+    setHistories([])
+    setStatus('inProgress')
+  }
+
+  const onPressEnter = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    if(e.nativeEvent.isComposing) {
+      return
+    }
+    if(e.key === 'Enter' && e.shiftKey) {
+      return
+    }
+    if(e.key === 'Enter') {
+      onNextClick()
+      return
+    }
+  }
+
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
-      {loading && "로딩중입니다..."}
-      <form onSubmit={onNextClick}>
-        <label htmlFor="answer">{question}</label>
-        <textarea
-          value={answer}
-          onChange={answerChangeHandler}
-          name="answer"
-          id="answer"
-          maxLength={200}
-          className="w-full px-3 py-2 mb-7 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-          placeholder="여기에 입력해주세요(200자 이내)"
-        />
-        <button type="submit" className="border rounded px-3">다음</button>
-      </form>
+      {status === 'ready' && (
+        <div className="flex flex-col items-center">
+        <p>당신이 하는 일 중 가장 중요한 단 하나, 그게 무엇인지 함께 찾아봐요</p>
+        <button className="btn" onClick={onStartClick}>시작하기</button>
+      </div>
+      )}
+      {status === 'inProgress' && (
+          <Fragment>
+            <div className="py-4">
+            {loading ? <div className="text-center"><span className="loading loading-dots"></span></div> : `Q. ${question}`}
+            </div>
+            A.
+            <div className="flex items-end gap-x-2">
+              <textarea
+                value={answer}
+                onChange={answerChangeHandler}
+                onKeyDown={onPressEnter}
+                maxLength={200}
+                className="textarea textarea-bordered w-full"
+                placeholder="여기에 입력해주세요(200자 이내)"
+              />
+              <button type="submit" className="btn btn-circle">Enter</button>
+            </div>
+          </Fragment>
+      )}
+      {status === 'end' && 
+      <div className="flex flex-col items-center">
+        <p>{question}</p>
+        <button onClick={onStartClick} className="btn">다시하기</button>
+      </div>}
     </div>
   );
 }
